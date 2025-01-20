ca6995802da82a1f1ac0f22ec90a40dc96e8d1e4
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2025-01-20 22:58:13 +0300

    Added option to see price per T1 attribute level in the auctioned Attribute Shard's lore, based on item's price (e.g. for T3 shard it shows its price divided by 4). Helps to compare prices for high-tier attribute shards on AH. [disabled by default]

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ import "./features/inventory/showArmorAttributes";
 import "./features/inventory/showFishingRodAttributes";
 import "./features/inventory/showRarityUpgrade";
 import "./features/inventory/highlightAttributeFusionMatchingItems";
+import "./features/inventory/showPricePerT1Shard";
 
 import "./features/chat/announceMobSpawnToAllChat";
 import "./features/chat/messageOnPlayerDeath";
