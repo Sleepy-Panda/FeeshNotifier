04174a1525f8483f5a424d21428082221aa85ad0
Sleepy-Panda <23345984+Sleepy-Panda@users.noreply.github.com>
2025-04-01 19:12:37 +0300

    refactor: Applied registerIf for all inventory features (#92)
    * Added registers.js
    
    * Added registerIf to inventory features
    
    * Refactored attributes rendering
    
    * Changelog

--- features/inventory/highlightAttributeFusionMatchingItems.js
+++ features/inventory/highlightAttributeFusionMatchingItems.js
@@ -1,6 +1,7 @@
 import settings from "../../settings";
 import { getItemAttributes } from "../../utils/common";
 import { isInSkyblock } from "../../utils/playerState";
+import { registerIf } from "../../utils/registers";
 
 const CRIMSON_ARMOR_NAMES = [
     'CRIMSON',
@@ -13,9 +14,10 @@ const CRIMSON_ARMOR_NAMES = [
 const TARGET_ITEM_SLOT_INDEX = 29;
 const COMBINED_ITEM_SLOT_INDEX = 13;
 
-register('renderSlot', (slot, gui, event) => {
-    highlightAttributeFusionMatchingItems(slot, gui);
-});
+registerIf(
+    register('renderSlot', (slot, gui, event) => highlightAttributeFusionMatchingItems(slot, gui)),
+    () => isInSkyblock() && settings.highlightMatchingItemsInAttributeFusion
+);
 
 function highlightAttributeFusionMatchingItems(slot, gui) {
     if (!settings.highlightMatchingItemsInAttributeFusion || !isInSkyblock()) {
