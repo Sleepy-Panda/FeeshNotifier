d0fc18707916b2ddf828b5b9ca05c1678d17d39f
SleepyPanda <23345984+Sleepy-Panda@users.noreply.github.com>
2025-10-18 15:46:35 +0300

    Fixed Flare & Umberella tracking

--- constants/javaTypes.js
+++ constants/javaTypes.js
@@ -5,7 +5,6 @@ export const EntityFireworkRocket = Java.type("net.minecraft.entity.projectile.F
 export const GuiInventory = Java.type("net.minecraft.client.gui.screen.ingame.InventoryScreen");
 export const GuiChat = Java.type("net.minecraft.client.gui.screen.ChatScreen");
 export const GuiChest = Java.type("net.minecraft.client.gui.screen.ingame.GenericContainerScreen");
-//export const EntityJoinWorldEvent = Java.type("net.minecraftforge.event.entity.EntityJoinWorldEvent");
 export const HandledScreen = Java.type("net.minecraft.client.gui.screen.ingame.HandledScreen");
 export const DataComponentTypes = Java.type("net.minecraft.component.DataComponentTypes");
 export const NbtOps = Java.type("net.minecraft.nbt.NbtOps");
